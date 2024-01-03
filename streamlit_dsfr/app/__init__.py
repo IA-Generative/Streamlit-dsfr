dcbdb1e9258cc7f719b7cacb8b8f7414899c5c17
Mathieu Guérin <mathieu.guerin@saegus.com>
2024-01-03 14:58:18 +0100

    Remove my_component declaration

--- streamlit_dsfr/app/__init__.py
+++ streamlit_dsfr/app/__init__.py
@@ -19,7 +19,6 @@ _RELEASE = (os.environ.get('APP_ENV') or 'prod') == 'prod'
 # best practice.
 
 supported_components = [
-    'my_component',
     'dsfr_button',
 ]
 
@@ -46,44 +45,6 @@ else:
                 path = os.path.join(build_dir, component),
             )
 
-
-# Create a wrapper function for the component. This is an optional
-# best practice - we could simply expose the component function returned by
-# `declare_component` and call it done. The wrapper allows us to customize
-# our component's API: we can pre-process its input args, post-process its
-# output value, and add a docstring for users.
-def my_component(name, key = None):
-    """Create a new instance of "my_component".
-
-    Parameters
-    ----------
-    name: str
-        The name of the thing we're saying hello to. The component will display
-        the text "Hello, {name}!"
-    key: str or None
-        An optional key that uniquely identifies this component. If this is
-        None, and the component's arguments are changed, the component will
-        be re-mounted in the Streamlit frontend and lose its current state.
-
-    Returns
-    -------
-    int
-        The number of times the component's "Click Me" button has been clicked.
-        (This is the value passed to `Streamlit.setComponentValue` on the
-        frontend.)
-
-    """
-    # Call through to our private component function. Arguments we pass here
-    # will be sent to the frontend, where they'll be available in an "args"
-    # dictionary.
-    #
-    # "default" is a special argument that specifies the initial return
-    # value of the component before the user has interacted with it.
-    component_value = _my_component_func(name = name, key = key, default = 0)
-
-    # We could modify the value returned from the component if we wanted.
-    # There's no need to do this in our simple example - but it's an option.
-    return component_value
 
 def dsfr_button(label, key = None):
 	component_value = _dsfr_button_func(label = label, key = key, default = False)
