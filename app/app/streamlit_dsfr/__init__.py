7329f590dce2f22b7317b311cc66f42cd8b982d9
Matiboux <matiboux@gmail.com>
2024-01-08 12:46:29 +0100

    Add more params in other components

--- app/app/streamlit_dsfr/__init__.py
+++ app/app/streamlit_dsfr/__init__.py
@@ -42,13 +42,13 @@ def dsfr_alert(
 	title_or_description: str,
 	description: str | None = None,
 	type: str | None = None,
-	*,
 	small: bool | None = None,
+	*,
 	closed: bool | None = None,
 	closeable: bool | None = None,
 	titleTag: str | None = None,
-	key: str | None = None,
 	id: str | None = None,
+	key: str | None = None,
 	**kwargs,
 ):
 	if description is not None:
@@ -72,22 +72,162 @@ def dsfr_alert(
 
 	return _dsfr_alert_func(**kwargs, key = key, default = None)
 
-def dsfr_badge(label, key = None):
-	component_value = _dsfr_badge_func(label = label, key = key, default = False)
-	return component_value
+def dsfr_badge(
+	label: str,
+	type: str | None = None,
+	small: bool | None = None,
+	*,
+	noIcon: bool | None = None,
+	ellipsis: bool | None = None,
+	key: str | None = None,
+	**kwargs,
+):
+	if type is not None:
+		kwargs['type'] = type
+	if small is not None:
+		kwargs['small'] = small
+	if noIcon is not None:
+		kwargs['noIcon'] = noIcon
+	if ellipsis is not None:
+		kwargs['ellipsis'] = ellipsis
 
-def dsfr_breadcrumb(label, items = None, key = None):
-	component_value = _dsfr_breadcrumb_func(label = label, items = items, key = key, default = False)
-	return component_value
+	return _dsfr_badge_func(label = label, **kwargs, key = key, default = None)
 
-def dsfr_button(label, key = None):
-	component_value = _dsfr_button_func(label = label, key = key, default = False)
-	return component_value
+def dsfr_breadcrumb(
+	links: str | list[str] | list[(str, str)] | list[dict[str, str]] | None = None,
+	*,
+	id: str | None = None,
+	key: str | None = None,
+	**kwargs,
+):
+	if links is not None:
+		if isinstance(links, str):
+			kwargs['links'] = [{'to': links, 'link': links}]
+		elif isinstance(links, list):
+			if len(links) <= 0:
+				kwargs['links'] = []
+			if isinstance(links[0], str):
+				kwargs['links'] = [{'to': link, 'link': link} for link in links]
+			elif isinstance(links[0], tuple):
+				kwargs['links'] = [{'to': link[0], 'link': link[1]} for link in links]
+			elif isinstance(links[0], dict):
+				kwargs['links'] = links
+			else:
+				raise ValueError('links must be a list of strings, tuples or dicts')
+		else:
+			raise ValueError('links must be a list of strings, tuples or dicts')
 
-def dsfr_checkbox(label, value = None, key = None):
-	component_value = _dsfr_checkbox_func(label = label, modelValue = value, key = key, default = False)
-	return component_value
+	if id is not None:
+		kwargs['breadcrumbId'] = id
 
-def dsfr_input(key = None):
-	component_value = _dsfr_input_func(key = key, default = False)
-	return component_value
+	return _dsfr_breadcrumb_func(**kwargs, key = key, default = False)
+
+def dsfr_button(
+	label: str,
+	size: str | None = None,
+	disabled: bool | None = None,
+	*,
+	secondary: bool | None = None,
+	tertiary: bool | None = None,
+	icon: str | None = None,
+	iconOnly: bool | None = None,
+	iconRight: bool | None = None,
+	noOutline: bool | None = None,
+	key: str | None = None,
+	**kwargs,
+):
+	if size is not None:
+		kwargs['size'] = size
+	if disabled is not None:
+		kwargs['disabled'] = disabled
+	if secondary is not None:
+		kwargs['secondary'] = secondary
+	if tertiary is not None:
+		kwargs['tertiary'] = tertiary
+	if icon is not None:
+		kwargs['icon'] = icon
+	if iconOnly is not None:
+		kwargs['iconOnly'] = iconOnly
+	if iconRight is not None:
+		kwargs['iconRight'] = iconRight
+	if noOutline is not None:
+		kwargs['noOutline'] = noOutline
+
+	return _dsfr_button_func(label = label, **kwargs, key = key, default = False)
+
+def dsfr_checkbox(
+	hint: str | None = None,
+	small: bool | None = None,
+	required: bool | None = None,
+	name: str | None = None,
+	*,
+	movelValue: bool | None = None,
+	id: str | None = None,
+	inline: bool | None = None,
+	errorMessage: str | None = None,
+	validMessage: str | None = None,
+	key: str | None = None,
+	**kwargs,
+):
+	if hint is not None:
+		kwargs['hint'] = hint
+	if small is not None:
+		kwargs['small'] = small
+	if required is not None:
+		kwargs['required'] = required
+	if name is not None:
+		kwargs['name'] = name
+	if movelValue is not None:
+		kwargs['movelValue'] = movelValue
+	if id is not None:
+		kwargs['id'] = id
+	if inline is not None:
+		kwargs['inline'] = inline
+	if errorMessage is not None:
+		kwargs['errorMessage'] = errorMessage
+	if validMessage is not None:
+		kwargs['validMessage'] = validMessage
+
+	return _dsfr_checkbox_func(**kwargs, key = key, default = False)
+
+def dsfr_input(
+	label: str,
+	hint: str | None = None,
+	value: str | None = None,
+	*,
+	labelVisible: bool | None = None,
+	id: str | None = None,
+	descriptionId: str | None = None,
+	isInvalid: bool | None = None,
+	isValid: bool | None = None,
+	isWithWarning: bool | None = None,
+	labelClass: str | None = None,
+	wrapperClass: str | None = None,
+	requiredTip: str | None = None,
+	key: str | None = None,
+	**kwargs,
+):
+	if hint is not None:
+		kwargs['hint'] = hint
+	if value is not None:
+		kwargs['modelValue'] = value
+	if labelVisible is not None:
+		kwargs['labelVisible'] = labelVisible
+	if id is not None:
+		kwargs['id'] = id
+	if descriptionId is not None:
+		kwargs['descriptionId'] = descriptionId
+	if isInvalid is not None:
+		kwargs['isInvalid'] = isInvalid
+	if isValid is not None:
+		kwargs['isValid'] = isValid
+	if isWithWarning is not None:
+		kwargs['isWithWarning'] = isWithWarning
+	if labelClass is not None:
+		kwargs['labelClass'] = labelClass
+	if wrapperClass is not None:
+		kwargs['wrapperClass'] = wrapperClass
+	if requiredTip is not None:
+		kwargs['requiredTip'] = requiredTip
+
+	return _dsfr_input_func(label = label, **kwargs, key = key, default = value)
