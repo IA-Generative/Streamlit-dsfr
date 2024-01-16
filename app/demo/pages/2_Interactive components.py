978752b3b025ef9f58d077da74c44b8517b0e657
Matiboux <matiboux@gmail.com>
2024-01-16 01:51:54 +0100

    Add columns to compare vanilla & dsfr components

--- app/demo/pages/2_Interactive components.py
+++ app/demo/pages/2_Interactive components.py
@@ -1,3 +1,6 @@
+import math
+import random
+
 import streamlit as st
 from streamlit_dsfr import \
 	dsfr_button, \
@@ -20,36 +23,78 @@ st.title('Interactive components')
 # Navigation menu
 nav_menu()
 
+col_left, col_right = st.columns(2)
+
+with col_left:
+	st.subheader('Vanilla components')
+
+with col_right:
+	st.subheader('DSFR components')
+
 # ---
 
 st.subheader('Buttons')
 
-bval = dsfr_button('This is a button')
-st.write(bval)
+col_left, col_right = st.columns(2)
+
+with col_left:
+	val = st.button('This is a button')
+	st.write(val)
+
+	val = st.button(f'This is a random number: {math.floor(random.random() * 100)}')
+	st.write(val)
+
+with col_right:
+	val = dsfr_button('This is a button')
+	st.write(val)
+
+	val = dsfr_button(f'This is a random number: {math.floor(random.random() * 100)}')
+	st.write(val)
 
 # ---
 
 st.subheader('Checkboxes')
 
-cval = dsfr_checkbox('This is a checkbox')
-st.write(cval)
+col_left, col_right = st.columns(2)
+
+with col_left:
+	val = st.checkbox('This is a checkbox')
+	st.write(val)
+
+with col_right:
+	val = dsfr_checkbox('This is a checkbox')
+	st.write(val)
 
 # ---
 
 st.subheader('Inputs')
 
-val = dsfr_input('This is an input')
-st.write(val)
+col_left, col_right = st.columns(2)
+
+with col_left:
+	val = st.text_input('This is an input')
+	st.write(val)
+
+with col_right:
+	val = dsfr_input('This is an input')
+	st.write(val)
 
 # ---
 
 st.subheader('Radios')
 
-val = dsfr_radio(['Option 1', 'Option 2', 'Option 3'])
-st.write(val)
+col_left, col_right = st.columns(2)
 
-val = dsfr_radio(['Small option 1', 'Small option 2', 'Small option 3'], small = True)
-st.write(val)
+with col_left:
+	val = st.radio('This is a radio', ['Option 1', 'Option 2', 'Option 3'])
+	st.write(val)
+
+with col_right:
+	val = dsfr_radio(['Option 1', 'Option 2', 'Option 3'])
+	st.write(val)
+
+	val = dsfr_radio(['Small option 1', 'Small option 2', 'Small option 3'], small = True)
+	st.write(val)
 
 # ---
 
