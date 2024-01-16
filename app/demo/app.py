abf618f78600e9946f81381d5caa58ea5cf4db53
Matiboux <matiboux@gmail.com>
2024-01-16 01:37:56 +0100

    Fix page links

--- app/demo/app.py
+++ app/demo/app.py
@@ -13,8 +13,8 @@ st.title('DSFR components demo')
 
 st.markdown(
 	"""
-- <a href="Static_components">Static components</a>
-- <a href="Interactive_components">Interactive components</a>
+- <a href="Static_components" target="_self">Static components</a>
+- <a href="Interactive_components" target="_self">Interactive components</a>
 	""",
 	unsafe_allow_html = True,
 )
