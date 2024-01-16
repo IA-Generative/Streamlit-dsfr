cd664691958589fc4359a2dd11a8a634aa6cf6ba
Matiboux <matiboux@gmail.com>
2024-01-16 01:29:02 +0100

    Add markdown links to pages

--- app/demo/app.py
+++ app/demo/app.py
@@ -3,3 +3,10 @@ import streamlit as st
 # ---
 
 st.title('DSFR components demo')
+
+st.markdown(
+	"""
+- [Static components](<Static_components>)
+- [Interactive components](<Interactive_components>)
+	""",
+)
