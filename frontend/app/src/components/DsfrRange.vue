b855e9650cb9b46380be47f1f7df96d7f70e9583
Matiboux <matiboux@gmail.com>
2024-01-19 14:43:31 +0100

    Add DsfrRange component

---
+++ frontend/app/src/components/DsfrRange.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Streamlit } from '~/stcomponentlib'
+import { DsfrRange } from '@gouvminint/vue-dsfr'
+
+import { useStreamlit } from '../streamlit'
+import type { ComponentProps } from '../types/ComponentProps.d.ts'
+
+useStreamlit()
+
+const props = defineProps<
+	ComponentProps<{
+		// Slots
+		label?: string
+		hint?: string
+		messages?: object
+		// Props
+		min?: number
+		max?: number
+		modelValue?: number
+		id?: string
+		lowerValue?: number
+		message?: string
+		prefix?: string
+		suffix?: string
+		small?: boolean
+		hideIndicators?: boolean
+		step?: number
+		disabled?: boolean
+	}>
+>()
+
+const lastValue = ref('')
+const value = ref('')
+
+// Bind the input value to `value`
+// Update the Steamlit value when:
+// - the input lose focus, if the value has changed
+// - the user press enter, if the input is focused and the value has changed
+
+const onInput = (event: Event) =>
+	{
+		const target = event.target as HTMLInputElement
+		value.value = target.value
+	}
+
+const onBlur = () =>
+	{
+		if (value.value !== lastValue.value)
+		{
+			lastValue.value = value.value
+			Streamlit.setComponentValue(value.value)
+		}
+	}
+
+const onKeydown = (event: KeyboardEvent) =>
+	{
+		if (event.key === 'Enter' && value.value !== lastValue.value)
+		{
+			lastValue.value = value.value
+			Streamlit.setComponentValue(value.value)
+		}
+	}
+</script>
+
+<template>
+	<div class="component">
+		<DsfrRange
+			v-bind="props.args"
+			:disabled="props.disabled || props.args.disabled"
+			v-model="value"
+			@input="onInput"
+			@blur="onBlur"
+			@keydown="onKeydown"
+		>
+			<template #label v-if="props.args.label">
+				{{ props.args.label }}
+			</template>
+			<template #required-tip v-if="props.args.requiredTip">
+				{{ props.args.requiredTip }}
+			</template>
+		</DsfrRange>
+	</div>
+</template>
+
+<!-- <style scoped>
+.component {
+	margin: 4px; /* Margin for the input outline on focus */
+}
+</style> -->
