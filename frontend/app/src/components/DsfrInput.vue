1c308d9ae1941f55a9134041c52a8bd067b6dc50
Matiboux <matiboux@gmail.com>
2024-01-22 17:30:15 +0100

    Refactor DsfrInput

--- frontend/app/src/components/DsfrInput.vue
+++ frontend/app/src/components/DsfrInput.vue
@@ -31,37 +31,37 @@ const props = defineProps<
 	}>
 >()
 
+// Bind the input value to `value`
 const value = ref<string>(props.args.modelValue || '')
 const lastValue = ref(value.value)
 
-// Bind the input value to `value`
 // Update the Steamlit value when:
 // - the input lose focus, if the value has changed
 // - the user press enter, if the input is focused and the value has changed
 
-const onInput = (event: Event) =>
-	{
-		const target = event.target as HTMLInputElement
-		value.value = target.value
-	}
+function onInput(event: Event)
+{
+	const target = event.target as HTMLInputElement
+	value.value = target.value
+}
 
-const onBlur = () =>
+function onBlur()
+{
+	if (value.value !== lastValue.value)
 	{
-		if (value.value !== lastValue.value)
-		{
-			lastValue.value = value.value
-			Streamlit.setComponentValue(value.value)
-		}
+		lastValue.value = value.value
+		Streamlit.setComponentValue(value.value)
 	}
+}
 
-const onKeydown = (event: KeyboardEvent) =>
+function onKeydown(event: KeyboardEvent)
+{
+	if (event.key === 'Enter' && value.value !== lastValue.value)
 	{
-		if (event.key === 'Enter' && value.value !== lastValue.value)
-		{
-			lastValue.value = value.value
-			Streamlit.setComponentValue(value.value)
-		}
+		lastValue.value = value.value
+		Streamlit.setComponentValue(value.value)
 	}
+}
 </script>
 
 <template>
