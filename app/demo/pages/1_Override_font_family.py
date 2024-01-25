fe20981bbccf5bdc0bde089a513d58d3c9f97afd
Matiboux <matiboux@gmail.com>
2024-01-25 14:11:42 +0100

    Refactor demo page

--- app/demo/pages/1_Override_font_family.py
+++ app/demo/pages/1_Override_font_family.py
@@ -1,18 +1,5 @@
-import math
-import random
-
 import streamlit as st
-from streamlit_dsfr import \
-	dsfr_button, \
-	dsfr_checkbox, \
-	dsfr_radio, \
-	dsfr_text_input, \
-	dsfr_number_input, \
-	dsfr_text_area, \
-	dsfr_date_input, \
-	dsfr_time_input, \
-	dsfr_range, \
-	override_font_family
+from streamlit_dsfr import override_font_family
 
 from disable_sidebar import disable_sidebar
 from nav_menu import nav_menu
@@ -32,9 +19,8 @@ nav_menu()
 # ---
 st.divider()
 
-with st.echo():
-	# CSS font family override
-	override_font_family()
+# CSS font family override
+override_font_family()
 
-	st.markdown('### This is a title')
-	st.markdown('This is a paragraph.')
+st.markdown('### This is a title')
+st.markdown('This is a paragraph.')
